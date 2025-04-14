01b4e0ede3e2a87016dcf5dd16a0032e3c67cb07
IzedeBaggio <baggio.izede@escola.pr.gov.br>
2025-04-13 22:20:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+document.getElementById("increase-font").addEventListener("click", function() {
+    let currentSize = window.getComputedStyle(document.body).fontSize;
+    currentSize = parseFloat(currentSize);
+    document.body.style.fontSize = (currentSize + 2) + "px";
+});
+
+document.getElementById("decrease-font").addEventListener("click", function() {
+    let currentSize = window.getComputedStyle(document.body).fontSize;
+    currentSize = parseFloat(currentSize);
+    document.body.style.fontSize = (currentSize - 2) + "px";
+=======
+document.getElementById("increase-font").addEventListener("click", function() {
+    let currentSize = window.getComputedStyle(document.body).fontSize;
+    currentSize = parseFloat(currentSize);
+    document.body.style.fontSize = (currentSize + 2) + "px";
+});
+
+document.getElementById("decrease-font").addEventListener("click", function() {
+    let currentSize = window.getComputedStyle(document.body).fontSize;
+    currentSize = parseFloat(currentSize);
+    document.body.style.fontSize = (currentSize - 2) + "px";
+>>>>>>> 2c79c0b612ab3b6b98ef93abe7d268c9fa0893c6
+});
